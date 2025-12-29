6f117d87975f16d5e5d8d2be857e7dfc8a1b6ce8
elunico <elunico@noreply.users.github.com>
2025-12-29 02:41:28 -0500

    removed deprecated function

--- uiutils.hpp
+++ uiutils.hpp
@@ -14,7 +14,7 @@ inline static ConfirmResult confirm(char const* fmt, Args&&... args)
 {
    char buf[1 << 12];
    std::snprintf(buf, 1 << 12, fmt, (args, ...));
-   auto result = fl_ask("%s", buf);
+   auto result = fl_choice("%s", fl_no, fl_yes, 0, buf);
    if (result == 1) {
      return ConfirmResult::YES; 
    }
